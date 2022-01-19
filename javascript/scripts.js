7269061484be1d372276a28e5411d37a776a1524
chris <19810192+xodus899@users.noreply.github.com>
2022-01-19 17:39:26 -0500

    make inputs required disable button

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,64 +1,79 @@
 //click add show form, toggle to hide, run checkforsubmit
 function showForm() {
-    const addBtn = document.querySelector("#book-btn");
-    const form = document.querySelector("#bookForm");
+  const addBtn = document.querySelector("#book-btn");
+  const form = document.querySelector("#bookForm");
 
     addBtn.addEventListener("click", () => {
-        if (form.classList.contains("hide")) {
-            form.classList.toggle("show");
-        }
+      if (form.classList.contains("hide")) {
+        form.classList.toggle("show");
+      }
     })
-    checkForSubmit()
+    checkForSubmit();
 }
+showForm();
 
 //check for submit -- run add book if fields are filled, no refresh
 function checkForSubmit() {
-    const button = document.querySelector("#submit");
+  const button = document.querySelector("#submit");
 
-    button.addEventListener("click", (event) => {
-        addBookToPage();
-        event.preventDefault();
-    })
+  button.addEventListener("click", (event) => {
+    addBookToPage();
+    event.preventDefault();
+  })
 }
 
 //loop through each node add to a single div, clear form after submit, 
 function addBookToPage() {
-    const mainDiv = document.querySelector("#main-book-div");
-    const allInput = document.querySelectorAll('input');
-    const div = document.createElement("div");
-    const button = document.createElement("button");
-    div.className = "book-div";
+  const mainDiv = document.querySelector("#main-book-div");
+  const allInput = document.querySelectorAll('input');
+  const div = document.createElement("div");
+  const button = document.createElement("button");
+  div.className = "book-div";
 
-    allInput.forEach((book) => {
-        const newDiv = document.createElement("div");
-        newDiv.textContent = `${book.name}: ${book.value}`;
-        div.appendChild(newDiv);
-    });
+  allInput.forEach((book) => {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = `${book.name}: ${book.value}`;
+    div.appendChild(newDiv);
+  });
 
-    function CreateDeleteButton() {
-        button.innerText = "Delete";
-        button.className = "delete";
-        button.addEventListener("click", deleteBook, false);
-    }
+  function CreateDeleteButton() {
+    button.innerText = "Delete";
+    button.className = "delete";
+    button.addEventListener("click", deleteBook, false);
+  }
 
-    resetForm();
-    CreateDeleteButton()
-    mainDiv.appendChild(div);
-    div.appendChild(button);
+  resetForm();
+  CreateDeleteButton()
+  mainDiv.appendChild(div);
+  div.appendChild(button);
 }
-
-
 
 //select form and reset on submit.
 function resetForm() {
-    const form = document.querySelector("#bookForm");
+  const form = document.querySelector("#bookForm");
 
-    form.classList.toggle("show");
-    form.reset();
+  form.classList.toggle("show");
+  form.reset();
 }
 
 function deleteBook(event) {
-    return event.target.closest(".book-div").remove();
+  return event.target.closest(".book-div").remove();
 }
 
-showForm();
+// ----- input field check -----
+
+const formDiv = document.querySelector('.form-div');
+
+function checkForEmptyInputs() {
+  const button = document.querySelector("#submit");
+  const inputs = [...formDiv.querySelectorAll('input')];
+  const error = document.querySelector('.error')
+
+  const isEmpty = inputs.some(input => !input.value);
+  button.disabled = isEmpty;
+  button.style.cursor = isEmpty ? 'not-allowed' : 'pointer';
+}
+
+formDiv.addEventListener('input', checkForEmptyInputs);
+
+checkForEmptyInputs();
